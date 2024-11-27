f30afc4162be83ef5cc5e5ba20c3e0b56f41380b
Josepeache7 <jotazavala4@gmail.com>
2024-11-27 01:42:20 -0500

    Capa Entidad

---
+++ CapaEntidad/EntAsistencia.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class EntAsistencia
+    {
+        public int IdAsistencia { get; set; } // Identificador único de la asistencia
+        public int IdEmpleado { get; set; } // Clave foránea que relaciona al empleado
+        public int IdGrupo { get; set; } // Identificador del grupo al que pertenece
+        public DateTime Fecha { get; set; } // Fecha de la asistencia
+        public bool Presente { get; set; } // Indica si el empleado estuvo presente (true) o ausente (false)
+
+    }
+}
---
+++ CapaEntidad/EntEmpleado.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    internal class EntEmpleado
+    {
+        public int IdEmpleado { get; set; } // Identificador único
+        public string Nombre { get; set; } // Nombre del empleado
+        public string Apellido { get; set; } // Apellido del empleado
+        public bool Activo { get; set; } = true; // Estado del empleado, activo o inhabilitado
+        public DateTime FechaContrato { get; set; } // Fecha de contratación
+        public decimal SueldoBase { get; set; } // Sueldo base del empleado
+        public decimal SueldoFinal { get; set; } // Sueldo final del empleado
+        public string TipoPago { get; set; } = "Mensual"; // Tipo de pago: 'Mensual' o 'Quincenal'
+        public int IdCargo { get; set; } // ID del cargo, clave foránea
+    }
+}
